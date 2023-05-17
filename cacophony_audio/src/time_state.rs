af2e8776eddc9d5096cf9b1232660d9ea1e52924
Esther Alter <alters@mit.edu>
2023-05-17 12:43:32 -0400

    removed explicit derive

--- cacophony_audio/src/time_state.rs
+++ cacophony_audio/src/time_state.rs
@@ -1,17 +1,8 @@
 /// Describes the state of audio playback.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Default)]
 pub struct TimeState {
     /// The current playback time in samples.
     pub time: Option<u64>,
     /// If true, we're playing music, as opposed to random user input.
     pub music: bool,
-}
-
-impl Default for TimeState {
-    fn default() -> Self {
-        Self {
-            time: None,
-            music: false,
-        }
-    }
 }
