55aa765e3889355d09de5aacf85fcf529c02bdd8
Esther Alter <alters@mit.edu>
2023-05-19 12:20:00 -0400

    fraction

--- cacophony_text/src/lib.rs
+++ cacophony_text/src/lib.rs
@@ -1,2 +1,17 @@
 mod tts;
 pub use self::tts::TTS;
+use cacophony_core::Fraction;
+
+/// Converts a fraction to a string.
+pub fn fraction(fraction: &Fraction) -> String {
+    if fraction.is_nan() {
+        return "NaN".to_string();
+    }
+    let numer = fraction.numer().unwrap();
+    match fraction.denom().unwrap() {
+        // If the denominator is 1, return a whole number.
+        1 => numer.to_string(),
+        // Format the
+        other => format!("{}/{}", numer, other),
+    }
+}
