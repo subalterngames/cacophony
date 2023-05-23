8756df910a5c6678eb8aaa2217906ed5602d248d
Esther Alter <alters@mit.edu>
2023-05-23 13:48:03 -0400

    select

--- io/src/piano_roll_panel/select.rs
+++ io/src/piano_roll_panel/select.rs
@@ -1,10 +1,7 @@
-use super::EditModeDeltas;
 use crate::get_tooltip_with_values;
 use crate::panel::*;
-use common::ini::Ini;
 use common::time::Time;
-use common::Zero;
-use common::{Fraction, Note, SelectMode, EDIT_MODES, MAX_NOTE, MAX_VOLUME, MIN_NOTE};
+use common::{Note, SelectMode};
 
 /// Select notes.
 pub(super) struct Select {}
@@ -35,7 +32,7 @@ impl Panel for Select {
     fn update(
         &mut self,
         state: &mut State,
-        conn: &mut Conn,
+        _: &mut Conn,
         input: &Input,
         tts: &mut TTS,
         text: &Text,
@@ -59,7 +56,89 @@ impl Panel for Select {
             };
             Some(UndoRedoState::from((s0, state)))
         } else if input.happened(&InputEvent::SubPanelTTS) {
-            panic!("TODO")
+            let mode = state.select_mode.clone();
+            let s = match mode {
+                SelectMode::Single(index) => {
+                    let selection = match index {
+                        Some(index) => match state.music.get_selected_track() {
+                            Some(track) => {
+                                let note = &track.notes[index];
+                                let time = text.get_time(&note.start, state.music.bpm);
+                                text.get_with_values(
+                                    "SELECT_TTS_SINGLE_SELECTION",
+                                    &[&note.note.to_string(), &note.velocity.to_string(), &time],
+                                )
+                            }
+                            None => text.get_with_values("ERROR", &["there is no track!"]),
+                        },
+                        None => text.get("SELECT_TTS_SINGLE_NO_SELECTION"),
+                    };
+                    get_tooltip_with_values(
+                        "SELECT_TTS_SINGLE",
+                        &[
+                            InputEvent::SelectStartLeft,
+                            InputEvent::SelectStartRight,
+                            InputEvent::SelectNone,
+                            InputEvent::SelectAll,
+                            InputEvent::PianoRollCycleMode,
+                        ],
+                        &[&selection],
+                        input,
+                        text,
+                    )
+                }
+                SelectMode::Many(indices) => {
+                    let selection = match indices {
+                        Some(indices) => match state.music.get_selected_track() {
+                            Some(track) => {
+                                let notes: Vec<&Note> = track
+                                    .notes
+                                    .iter()
+                                    .enumerate()
+                                    .filter(|n| indices.contains(&n.0))
+                                    .map(|n| n.1)
+                                    .collect();
+                                match notes.iter().min() {
+                                    Some(min) => match notes.iter().max() {
+                                        Some(max) => {
+                                            let t0 = text.get_time(&min.start, state.music.bpm);
+                                            let t1 = text.get_time(&max.start, state.music.bpm);
+                                            text.get_with_values(
+                                                "SELECT_TTS_MANY_SELECTION",
+                                                &[&t0, &t1],
+                                            )
+                                        }
+                                        None => text
+                                            .get_with_values("ERROR", &["there is no end time!"]),
+                                    },
+                                    None => {
+                                        text.get_with_values("ERROR", &["there is no start time!"])
+                                    }
+                                }
+                            }
+                            None => text.get_with_values("ERROR", &["there is no track!"]),
+                        },
+                        None => text.get("SELECT_TTS_MANY_NO_SELECTION"),
+                    };
+                    get_tooltip_with_values(
+                        "SELECT_TTS_MANY",
+                        &[
+                            InputEvent::SelectStartLeft,
+                            InputEvent::SelectStartRight,
+                            InputEvent::SelectEndLeft,
+                            InputEvent::SelectEndRight,
+                            InputEvent::SelectNone,
+                            InputEvent::SelectAll,
+                            InputEvent::PianoRollCycleMode,
+                        ],
+                        &[&selection],
+                        input,
+                        text,
+                    )
+                }
+            };
+            tts.say(&s);
+            None
         }
         // Move the selection start leftwards.
         else if input.happened(&InputEvent::SelectStartLeft) {
@@ -160,7 +239,7 @@ impl Panel for Select {
                     Some(indices) => match indices.len() > 1 {
                         // Remove an index.
                         true => {
-                            let indices = indices.as_slice()[0..indices.len()].to_vec();
+                            let indices = indices.as_slice()[0..indices.len() - 1].to_vec();
                             state.select_mode = SelectMode::Many(Some(indices));
                             return Some(UndoRedoState::from((s0, state)));
                         }
@@ -184,6 +263,113 @@ impl Panel for Select {
                 },
             }
             return None;
+        }
+        // Deselect.
+        else if input.happened(&InputEvent::SelectNone) {
+            let s0 = state.clone();
+            let mode = state.select_mode.clone();
+            state.select_mode = match mode {
+                SelectMode::Single(_) => SelectMode::Single(None),
+                SelectMode::Many(_) => SelectMode::Many(None),
+            };
+            Some(UndoRedoState::from((s0, state)))
+        }
+        // Select all.
+        else if input.happened(&InputEvent::SelectAll) {
+            match state.music.get_selected_track() {
+                Some(track) => {
+                    let indices = track.notes.iter().enumerate().map(|n| n.0).collect();
+                    let s0 = state.clone();
+                    state.select_mode = SelectMode::Many(Some(indices));
+                    Some(UndoRedoState::from((s0, state)))
+                }
+                None => None,
+            }
+        }
+        // Adjust the end of the selection.
+        else if let SelectMode::Many(indices) = &state.select_mode {
+            // Remove a note at the end.
+            if input.happened(&InputEvent::SelectEndLeft) {
+                match indices {
+                    Some(indices) => match indices.len() > 1 {
+                        true => {
+                            let s0 = state.clone();
+                            let indices = indices.as_slice()[0..indices.len() - 1].to_vec();
+                            state.select_mode = SelectMode::Many(Some(indices));
+                            Some(UndoRedoState::from((s0, state)))
+                        }
+                        false => {
+                            let s0 = state.clone();
+                            state.select_mode = SelectMode::Many(None);
+                            Some(UndoRedoState::from((s0, state)))
+                        }
+                    },
+                    None => {
+                        if let Some(track) = state.music.get_selected_track() {
+                            if let Some(index) = Select::get_note_index_closest_to_after_cursor(
+                                &track.notes,
+                                &state.time,
+                            ) {
+                                let s0 = state.clone();
+                                state.select_mode = SelectMode::Many(Some(vec![index]));
+                                return Some(UndoRedoState::from((s0, state)));
+                            }
+                        }
+                        None
+                    }
+                }
+            }
+            // Add a note at the end.
+            else if input.happened(&InputEvent::SelectEndRight) {
+                match indices {
+                    Some(indices) => match indices.iter().max() {
+                        Some(max) => {
+                            if let Some(track) = state.music.get_selected_track() {
+                                if let Some(max) = track
+                                    .notes
+                                    .iter()
+                                    .enumerate()
+                                    .filter(|n| n.1.gt(&track.notes[*max]))
+                                    .max_by(|a, b| a.1.cmp(b.1))
+                                {
+                                    let mut indices = indices.clone();
+                                    indices.push(max.0);
+                                    let s0 = state.clone();
+                                    state.select_mode = SelectMode::Many(Some(indices));
+                                    return Some(UndoRedoState::from((s0, state)));
+                                }
+                            }
+                        }
+                        None => {
+                            if let Some(track) = state.music.get_selected_track() {
+                                if let Some(index) = Select::get_note_index_closest_to_after_cursor(
+                                    &track.notes,
+                                    &state.time,
+                                ) {
+                                    let s0 = state.clone();
+                                    state.select_mode = SelectMode::Many(Some(vec![index]));
+                                    return Some(UndoRedoState::from((s0, state)));
+                                }
+                            }
+                        }
+                    },
+                    None => {
+                        if let Some(track) = state.music.get_selected_track() {
+                            if let Some(index) = Select::get_note_index_closest_to_after_cursor(
+                                &track.notes,
+                                &state.time,
+                            ) {
+                                let s0 = state.clone();
+                                state.select_mode = SelectMode::Many(Some(vec![index]));
+                                return Some(UndoRedoState::from((s0, state)));
+                            }
+                        }
+                    }
+                }
+                None
+            } else {
+                None
+            }
         } else {
             None
         }
