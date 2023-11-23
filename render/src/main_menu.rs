89fe0ac45f7c68b5d270103395527fdf2ed4c392
Seth Alter <alters@mit.edu>
2023-11-23 13:43:55 -0500

    tiny optimization

--- render/src/main_menu.rs
+++ render/src/main_menu.rs
@@ -280,8 +280,8 @@ impl MainMenu {
     /// Get a sample, set lerp targets, and draw bars.
     pub fn late_update(&mut self, renderer: &Renderer, conn: &Conn) {
         // Set the power bar lerp targets from the sample.
-        let sample = *conn.sample.lock();
         if self.time % POWER_BAR_DELTA == 0 {
+            let sample = *conn.sample.lock();
             self.set_lerp_target(0, sample.0);
             self.set_lerp_target(1, sample.1);
         }
