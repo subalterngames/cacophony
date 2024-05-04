e53ebd31dc1ca5b3102455711a827d56630609db
subalterngames <subalterngames@gmail.com>
2024-05-04 10:45:20 -0400

    Fixed a CSV error. Added a new test

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,11 @@ async fn main() {
     // Parse and load the command line arguments.
     let args = Args::parse();
 
+    // Immediately quit.
+    if args.quit {
+        return;
+    }
+
     // Get the paths, initialized in loading the window configuration.
     let paths = Paths::get();
 
